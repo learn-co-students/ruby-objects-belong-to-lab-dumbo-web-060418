93c73e7a8351832345deb57ef345f27a5d228f68
robertexe <robtabares92@gmail.com>
2018-06-07 12:27:18 -0400

    Done.

---
+++ lib/helper.rb
@@ -0,0 +1,11 @@
+# require 'pry'
+# require_relative "../lib/artist.rb"
+# require_relative "../lib/song.rb"
+# require_relative "../lib/author.rb"
+# require_relative "../lib/post.rb"
+#
+#
+#
+#
+#
+# binding.pry
