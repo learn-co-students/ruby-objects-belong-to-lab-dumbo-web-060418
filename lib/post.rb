694562eba0ff6475ee81f700bf51dd2814bcfe19
jtx007 <jamesjacobthomas7@gmail.com>
2018-06-13 18:37:06 -0400

    Done.

--- lib/post.rb
+++ lib/post.rb
@@ -1,8 +1,6 @@
 class Post
-  attr_accessor :title, :author
 
-  def initialize
-    @title = title
-  end
+
+  attr_accessor :title, :author
 
 end
---
+++ lib/run.rb
@@ -0,0 +1,8 @@
+require_relative "./song.rb"
+require_relative "./author.rb"
+require_relative "./post.rb"
+require_relative "./artist.rb"
+
+require "pry"
+
+binding.pry
